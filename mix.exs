78a5a12fc145c48c2f16463ace77ea2f572ab6ad
Doug ClymerOlson <doug@yarina.org>
2024-07-10 20:12:45 +0000

    updated mix app name to fnxml

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule XMLStreamTools.MixProject do
 
   def project do
     [
-      app: :xml_stream_tools,
+      app: :fnxml,
       version: "0.1.0",
       elixir: "~> 1.15",
       start_permanent: Mix.env() == :prod,
